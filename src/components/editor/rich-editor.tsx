b936a4ddced0b93d97cc538ed6bbcf1386cd403c
Bilarius <bilariuss@yandex.ru>
2026-07-25 10:04:27 +0000

    fix: useState в rich-editor

--- src/components/editor/rich-editor.tsx
+++ src/components/editor/rich-editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useState }; import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Image from "@tiptap/extension-image";
 import Placeholder from "@tiptap/extension-placeholder";
